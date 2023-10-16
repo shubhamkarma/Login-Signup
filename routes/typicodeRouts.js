f59864b5a85bc6a32f346bf83688e4df4ed7aaf5
Shubham Karma <shubhamkarma2012@gmail.com>
2023-10-17 00:33:04 +0530

    auth in typicode

--- routes/typicodeRouts.js
+++ routes/typicodeRouts.js
@@ -1,8 +1,9 @@
 const {Router} = require('express')
 const typicodeController = require('../controller/typicodeController')
+const authentication = require('../middleware/authenticateToken')
 
 const router = Router()
 
-router.get('/posts', typicodeController.posts)
+router.get('/posts',authentication, typicodeController.posts)
 
 module.exports = router
